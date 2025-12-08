f74d240fe290db242bfc63eaed368cfec60d18b8
Sivakumar.Mopuri <siva.mopuri21@gmail.com>
2025-12-08 05:45:36 +0530

    edit .gitignore

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="document-portal",
+    author="Siva Kumar Mopuri",
+    license="MIT",
     version="0.1.0",
     packages=find_packages(),
-)
+)   
