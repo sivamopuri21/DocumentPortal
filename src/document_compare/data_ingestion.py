83e468304a9f0e0ab8cb1bc63ddc8c9a70b1136f
Sivakumar.Mopuri <siva.mopuri21@gmail.com>
2025-12-23 19:23:22 +0530

    code updated for reading pdf

--- src/document_compare/data_ingestion.py
+++ src/document_compare/data_ingestion.py
@@ -5,12 +5,47 @@ from logger.custom_logger import CustomLogger
 from exception.custom_exception import DocumentPoratalException
 
 class DocumentComparator:
-    def __init__(self):
-        pass
+    def __init__(self,base_dir):
+        self.log = CustomLogger().get_logger(__name__)
+        self.base_dir = Path(base_dir)
+        self.base_dir.mkdir(parents=True, exist_ok=True)
+
+
     def delete_existing_files(self):
-        pass
+        """Deletes existing files at the specific location """
+        try:
+            pass
+        except Exception as e:
+            self.log.error(f"Error deleting existing files: {e}")
+            raise DocumentPoratalException("Error deleting existing files", sys)
+        
+
     def save_uploded_files(self):
-        pass
-    def read_pdf(self):
-        pass
-    
+        """Saves uploaded files to a specific directory"""
+        try:
+            pass
+        except Exception as e:
+            self.log.error(f"Error saving uploaded files: {e}")
+            raise DocumentPoratalException("Error saving uploaded files", sys)
+    def read_pdf(self,pdf_path:Path) -> str:
+        """Reads a PDF file and extract text from each page"""
+        try:
+            with fitz.open(pdf_path) as doc:
+                if doc.is_encrypted:
+                    raise ValueError("PDF is encrypted and cannot be read : {pdf_path.name}")
+                
+                all_text = []
+                for page_num in range(doc.page_count):
+                    page = doc.load_page(page_num)
+                    text = page.get_text() #type: ignore
+
+                    if text.strip():
+                        all_text.append(f"\n --- Page {page_num + 1} --- \n{text}")
+
+                self.log.info("PDF read successfully",file=str(pdf_path),pages=len(all_text))
+                return "\n".join(all_text)
+
+                
+        except Exception as e:
+            self.log.error(f"Error reading PDF: {e}")
+            raise DocumentPoratalException("Error reading PDF",sys)
