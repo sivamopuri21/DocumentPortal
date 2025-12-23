cd1eecf825066d3ba8bff686b2ab892d7a0e0039
Sivakumar.Mopuri <siva.mopuri21@gmail.com>
2025-12-23 11:55:31 +0530

    document analyzer added

---
+++ src/document_analysier/data_ingestion.py
@@ -0,0 +1,20 @@
+import os
+import fitz
+import uuid
+from datetime import datetime
+from logger.custom_logger import CustomLogger
+from exception.custom_exception import DocumentPoratalException
+
+class DocumentHandler:
+    """
+    Handles PDF saving and readingf operations.
+    Automatically logs all actions and supports session-based organization.    
+    """
+    def __init__(self):
+        pass
+
+    def save_pdf(self):
+        pass
+
+    def read_pdf(self):
+        pass
