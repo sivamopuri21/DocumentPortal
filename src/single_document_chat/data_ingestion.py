7b17f1450aa2141be17244e85e90b96c7dcaf396
Sivakumar.Mopuri <siva.mopuri21@gmail.com>
2025-12-24 15:08:49 +0530

    single doc changes updated

--- src/single_document_chat/data_ingestion.py
+++ src/single_document_chat/data_ingestion.py
@@ -7,25 +7,59 @@ from langchain_community.vectorstores import FAISS
 from logger.custom_logger import CustomLogger
 from exception.custom_exception import DocumentPoratalException
 from utils.model_loader import ModelLoader
+from datetime import datetime
 
 class SingleDocIngestor:
-    def __init__(self):
+    def __init__(self,data_dir: str="data/single_document_chat",faiss_dir:str="faiss_index"):
         try:
             self.log = CustomLogger().get_logger(__name__)
+            self.data_dir = Path(data_dir)
+            self.data_dir.mkdir(parents=True, exist_ok=True)
+
+            self.faiss_dir = Path(faiss_dir)
+            self.faiss_dir.mkdir(parents=True, exist_ok=True)
+
+            self.model_loader = ModelLoader()
+            self.log.info("SingleDocIngestor initialized successfully",temp_path=str(self.data_dir), faiss_dir = str(self.faiss_dir))
+
         except Exception as e:
             self.log.error(f"Error initializing SingleDocIngestor",error=str(e))
             raise DocumentPoratalException("Error initializing SingleDocIngestor",sys)
         
-    def ingest_files(self):
+    def ingest_files(self,uploaded_files):
         try:
-            pass
+            documents=[]
+            for uploaded_file in uploaded_files:
+                unique_filename = f"session_{datetime.utcnow().strftime('%Y%m%d_%H%M%S')}_{uuid.uuid4().hex[:8]}.pdf"
+                temp_path = self.data_dir / unique_filename
+                with open(temp_path, "wb") as f:
+                    f.write(uploaded_file.read())
+                self.log.info(f"PDF saved for ingestion", file=uploaded_file.name)
+                loader = PyPDFLoader(str(temp_path))
+                docs = loader.load()
+                documents.extend(docs)
+            self.log.info("PDF files loaded",count=len(documents))
+            return self._create_retriver(documents)
+                
         except Exception as e:
             self.log.error(f"Error ingesting files", error=str(e))
             raise DocumentPoratalException("Error ingesting files", sys)
 
-    def _create_retriver(self):
+    def _create_retriver(self,documents):
         try:
-            pass
+            splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=300)
+            chunks = splitter.split_documents(documents)
+            self.log.info("Documents split into chunks", count=len(chunks))
+
+            embeddings = self.model_loader.load_embeddings()
+            vectorstore = FAISS.from_documents(chunks, embeddings)
+
+            vectorstore.save_local(str(self.faiss_dir))
+            self.log.info("Vectorstore created and saved", faiss_dir=str(self.faiss_dir))
+            retriever = vectorstore.as_retriever(search_kwargs={"k": 3},search_type="similarity")
+            self.log.info("Retriever created successfully", retriever_type=str(type(retriever)))
+            return retriever
+        
         except Exception as e:
             self.log.error(f"Error creating retriever", error=str(e))
             raise DocumentPoratalException("Error creating retriever", sys)
--- src/single_document_chat/retrieval.py
+++ src/single_document_chat/retrieval.py
@@ -1,11 +1,12 @@
 import os
 import sys
+import streamlit as st
 from dotenv import load_dotenv
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_community.vectorstores import FAISS
 from langchain_core.runnables.history import RunnableWithMessageHistory
-from langchain.chains import create_history_aware_retriver, create_retrival_chain
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from utils.model_loader import ModelLoader
 from exception.custom_exception import DocumentPoratalException
@@ -14,37 +15,85 @@ from prompt.prompt_library import PROMPT_REGISTRY
 from model.models import PromptType
 
 class ConversationalRAG:
-    def __init__(self,session_id:str,retriever) -> None:
+    def __init__(self,session_id:str,retriever):
         try:
             self.log = CustomLogger().get_logger(__name__)
+            self.session_id = session_id
+            self.retriver = retriever
+            self.llm=self._load_llm()
+            self.contextualize_prompt = PROMPT_REGISTRY[PromptType.CONTEXTUALIZE_QUESTION.value]
+            self.qa_prompt = PROMPT_REGISTRY[PromptType.CONTEXT_QA.value]
+            self.history_aware_retriever = create_history_aware_retriever(
+                self.llm,
+                self.retriver,
+                self.contextualize_prompt
+            )
+            self.log.info("Cretated history-aware retriver",session = session_id)
+            self.qa_chain = create_stuff_documents_chain(self.llm, self.qa_prompt)
+            self.rag_chain = create_retrieval_chain(self.history_aware_retriever, self.qa_chain)
+            self.log.info("Created RAG chain", session = session_id)
+            self.chain = RunnableWithMessageHistory(
+                self.rag_chain,
+                self._get_session_history,
+                input_messages_key="input",
+                history_messages_key="chat_history",
+                output_messages_key="answer"
+            )
+            self.log.info("Created RunnableWithMessageHistory", session = session_id)
+
         except Exception as e:
             self.log.error("Error initializing ConversationalRAG", error=str(e), session=session_id)
             raise DocumentPoratalException("Error initializing ConversationalRAG",sys)
         
     def _load_llm(self):
         try:
-            pass
+            llm = ModelLoader().load_llm()
+            self.log.info("LLM loaded successfully", class_name=llm.__class__.__name__)
+            return llm
         except Exception as e:
             self.log.error("Error loading LLM", error=str(e),session=self.session_id)
             raise DocumentPoratalException("Error loading LLM", sys)
         
-    def _get_session_history(self):
+    def _get_session_history(self, session_id: str) -> BaseChatMessageHistory:
         try:
-            pass
+            if "store" not in st.session_state:
+                st.session_state.store = {}
+
+            if session_id not in st.session_state.store:
+                st.session_state.store[session_id] = ChatMessageHistory()
+                self.log.info("New chat session history created", session_id=session_id)
+
+            return st.session_state.store[session_id]
         except Exception as e:
             self.log.error("Error getting session history", error=str(e),session=self.session_id)
             raise DocumentPoratalException("Error getting session history", sys)
         
-    def load_retriver_from_faiss(self):
+    def load_retriver_from_faiss(self, index_path: str):
         try:
-            pass
+            embeddings = ModelLoader().load_embeddings()
+            if not os.path.isdir(index_path):
+                raise FileNotFoundError(f"FAISS index directory not found: {index_path}")
+
+            vectorstore = FAISS.load_local(index_path, embeddings)
+            self.log.info("Loaded retriever from FAISS index", index_path=index_path)
+            return vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 5})
         except Exception as e:
             self.log.error("Error loading retriever from FAISS", error=str(e),session=self.session_id)
             raise DocumentPoratalException("Error loading retriever from FAISS", sys)
         
-    def invoke(self):
+    def invoke(self,user_input: str) -> str:
         try:
-            pass
+            response = self.chain.invoke(
+                {"input": user_input},
+                config={"configurable": {"session_id": self.session_id}}
+            )
+            answer = response.get("answer", "No answer.")
+
+            if not answer:
+                self.log.warning("Empty answer received", session_id=self.session_id)
+
+            self.log.info("Chain invoked successfully", session_id=self.session_id, user_input=user_input, answer_preview=answer[:150])
+            return answer
         except Exception as e:
             self.log.error("Error invoking ConversationalRAG", error=str(e), session=self.session_id)
             raise DocumentPoratalException("Error invoking ConversationalRAG", sys)
